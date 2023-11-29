d8b787a0ce7b003e84faa17c816fc51cd8de705a
LOUIS <louishognon@outlook.fr>
2023-11-29 11:44:40 +0100

    New plot1

--- plot1.R
+++ plot1.R
@@ -37,7 +37,6 @@ hpc$datetime <- strptime(paste(hpc$Date, hpc$Time),format = '%d/%m/%Y %H:%M:%S')
 hist(hpc$Global_active_power, col="red", xlab="Global Active Power (kilowatts",
      main = "Global Active Power")
 
-
 #Create a new factor datatime with Date and Time
 dev.copy(png, file = "plot1.png", height = 480, width = 480)
 dev.off()
